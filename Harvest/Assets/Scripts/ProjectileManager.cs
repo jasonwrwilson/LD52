38a0c3e42908876de36f8a9108d15b3d02e40801
jasonwrwilson <jason.wr.wilson@gmail.com>
2023-01-07 14:08:58 -0500

    New Weapon Scythe
    Spinning Scythe Weapon

--- Harvest/Assets/Scripts/ProjectileManager.cs
+++ Harvest/Assets/Scripts/ProjectileManager.cs
@@ -5,36 +5,34 @@ using UnityEngine;
 public class ProjectileManager : MonoBehaviour
 {
     private ProjectilePool projectilePool;
-    [SerializeField] private float projectileSpawnTime;
-    private float nextProjectileSpawnTime;
+    [SerializeField] private float scytheSpawnTime;
+    private float nextScytheSpawnTime;
+
+    [SerializeField] private int startingScytheCount;
+    private int scytheCount;
 
     [SerializeField] private Vector2 projectileOffset;
 
     // Start is called before the first frame update
     void Start()
     {
-        projectilePool = gameObject.GetComponent<ProjectilePool>();
-
-        nextProjectileSpawnTime = projectileSpawnTime;
+        nextScytheSpawnTime = scytheSpawnTime;
+        scytheCount = startingScytheCount;
+        
+        projectilePool = gameObject.GetComponent<ProjectilePool>();        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (nextProjectileSpawnTime > 0)
+        if (nextScytheSpawnTime > 0 && scytheCount > 0)
         {
-            nextProjectileSpawnTime -= Time.deltaTime;
-            if (nextProjectileSpawnTime <= 0)
+            nextScytheSpawnTime -= Time.deltaTime;
+            if (nextScytheSpawnTime <= 0)
             {
+                FireScythe();
 
-
-                //random direction
-                /*float angleInRadians = Random.Range(0, 360) * Mathf.Deg2Rad;
-                projectile.SetDirection(new Vector2(Mathf.Cos(angleInRadians), Mathf.Sin(angleInRadians)));*/
-                //FireBullet();
-
-
-                nextProjectileSpawnTime = projectileSpawnTime;
+                nextScytheSpawnTime = scytheSpawnTime;
             }
         }
     }
@@ -53,5 +51,24 @@ public class ProjectileManager : MonoBehaviour
         Vector2 mouseDirection = new Vector2(mousePosition.x - firePosition.x, mousePosition.y - firePosition.y);
         mouseDirection.Normalize();
         projectile.SetDirection(mouseDirection);
+    }
+
+    public void FireScythe()
+    {
+        //fire position
+        Vector3 scarecrowPosition = gameObject.transform.position;
+        Vector3 firePosition = new Vector3(scarecrowPosition.x + projectileOffset.x, scarecrowPosition.y + projectileOffset.y, 0);
+
+        float randomAngle = Random.Range(0, 360);
+        
+        for (int i = 0; i < scytheCount; i++)
+        {
+            //spawn scythe
+            ProjectileBehaviour projectile = projectilePool.GetProjectile(1, firePosition);
+
+            //random direction
+            float angleInRadians = (randomAngle + i * (360 / scytheCount)) * Mathf.Deg2Rad;
+            projectile.SetDirection(new Vector2(Mathf.Cos(angleInRadians), Mathf.Sin(angleInRadians)));
+        }
     }
 }
---
+++ Harvest/Assets/Scripts/ProjectileScythe.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScythe : ProjectileBehaviour
+{
+    [SerializeField] float rotationSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        base.Update();
+
+        Vector3 pos = gameObject.transform.position;
+
+        float degrees = rotationSpeed * Time.deltaTime;
+        
+        float sin = Mathf.Sin(degrees * Mathf.Deg2Rad);
+        float cos = Mathf.Cos(degrees * Mathf.Deg2Rad);
+
+        float tx = direction.x;
+        float ty = direction.y;
+
+        direction.x = (cos * tx) - (sin * ty);
+        direction.y = (sin * tx) + (cos * ty);
+
+        pos.x = pos.x + direction.x * speed * Time.deltaTime;
+        pos.y = pos.y + direction.y * speed * Time.deltaTime;
+
+        gameObject.transform.position = pos;
+
+    }
+
+}
